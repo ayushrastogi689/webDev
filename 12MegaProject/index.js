d51e11bbfaab042c221c3426d51c139aca9260e1
Ayush Rastogi <66988298+ayushrastogi689@users.noreply.github.com>
2023-01-13 20:48:51 +0530

    Deleted

--- 12MegaProject/index.js
+++
@@ -1,7 +0,0 @@
-const navLinks = document.getElementById("nav-links-id");
-
-const toggleButton = document.getElementById("nav-menu-toggle");
-
-toggleButton.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
